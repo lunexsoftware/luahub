5442f9514a0b7be01b9e73e93a8b357c80b67add
lunexsoftware <tesexrahyrezerved@gmail.com>
2026-05-18 18:06:08 +0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet(“https://raw.githubusercontent.com/lunexsoftware/luahub/refs/heads/main/script”))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/lunexsoftware/luahub/refs/heads/main/script"))()
